3b712bd8ae947e5848ef241866317c270bb23ab4
SimonePaglino <simone.paglino@rimax.io>
2020-06-09 20:34:13 +0200

    Last javadoc

--- F/Proxy.java
+++ F/Proxy.java
@@ -1,17 +1,27 @@
 import java.net.*;
 import java.io.*;
 
+/**
+* Classe utilizzata per scaricare la pagina HTML di una pagina WEB in base all'URL passato nel construttore
+* e che permette di astrarre la parte di connessione e ricezione dei dati
+*/
 public class Proxy implements ProxyInterface {
     private Socket socket;
     private BufferedReader reader;
     private PrintWriter writer;
 
+    /**
+    * Costruttore della classe Proxy
+    *
+    * @param url URL della pagina WEB da scaricare
+    */
     public Proxy(String url) {
         try {
             URI uri = new URI(url);
             InetAddress addr = InetAddress.getByName(uri.getHost());
+            // creo una nuova istanza della classe Socket passando l'host e la relativa a cui connettersi
             socket = new Socket(addr, ProxyInterface.PORT);
-            // crea gli stream di input/output
+            // creo gli stream di input e output
             reader = new BufferedReader(
                         new InputStreamReader(
                             socket.getInputStream()));
@@ -30,6 +40,11 @@ public class Proxy implements ProxyInterface {
         }
     }
 
+    /**
+    * Metodo che restituisce il contenuto HTML della pagina il cui URL è stato passato come parametro
+    *
+    * @param url URL della pagina WEB da scaricare
+    */
     public String setWebPageByProxy(String url) {
         String firstLineHTMLPage = "";
 
@@ -37,16 +52,17 @@ public class Proxy implements ProxyInterface {
             URI uri = new URI(url);
             String host = uri.getHost();
             String path = uri.getRawPath();
-            // Richiesta oggetto
+            // Effettuo la richiesta per ottenere il contenuto HTML della pagina
             writer.print(
                 "GET " + path + " HTTP/1.1\r\n" +
                 "Host: " + host + "\r\n" +
                 "Connection: close\r\n\r\n"
             );
+            // pulisco residui
             writer.flush();
-            // Risposta
-
+            
             String line;
+            // leggo tutta la pagina aggiornando il contenuto di firstLineHTMLPage (che conterrà l'intera pagina)
             while ((line = reader.readLine()) != null) {
                 firstLineHTMLPage += line + "\n";
             }
@@ -60,12 +76,14 @@ public class Proxy implements ProxyInterface {
         } finally {
             System.out.println("*** closing...");
             try {
+                // se la socket non è stata inizializzata o se è già stata chiusa, chiudo la connessione
                 if(socket != null && !socket.isClosed()) {
                     socket.close();
                 }
             } catch(IOException exc) {
                 System.err.println("ERROR error while closing socket");
             }
+            // restituisco il valore della pagina HTML
             return firstLineHTMLPage;
         }
     }
--- F/ProxyInterface.java
+++ F/ProxyInterface.java
@@ -1,5 +1,13 @@
+/**
+* Interfacccia implementata dalla classe Proxy
+*/
 public interface ProxyInterface {
+    // porta a cui connettersi per scaricare una pagina WEB
     public static final int PORT = 80;
-
+    /**
+    * Metodo che restituisce il contenuto HTML della pagina il cui URL è stato passato come parametro
+    *
+    * @param url URL della pagina WEB da scaricare
+    */
     public String setWebPageByProxy(String url);
 }
