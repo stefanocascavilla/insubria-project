a7378acfa2899706f3037cc126e5dff165d72754
SimonePaglino <simone.paglino@rimax.io>
2020-06-09 17:06:29 +0200

    Implements Server registry

--- F/F.java
+++ F/F.java
@@ -1,4 +1,11 @@
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+
 public class F {
+    private 
+
     public static void main(String[] args) {
         try {
             String URLWebsite = args[0];
@@ -7,7 +14,15 @@ public class F {
             if(webPageContent != null) {
                 WebsiteInfo contentSite = new WebsiteInfo(URLWebsite, webPageContent);
                 System.out.println(contentSite.getWebPageContentHTML());
-                // loadPage()
+                // 
+                System.setSecurityManager(new RMISecurityManager());
+                try {
+                    Registry registro = LocateRegistry.getRegistry("192.168.1.102", 1099);
+                    HelloPerson stub = (RemoteS) registro.lookup("sObj");
+                    stub.loadPage(contentSite);
+                } catch (Exception e) {
+                    System.err.println("Client exception: " + e.toString());
+                }
             } else {
                 System.err.println("The content of the web page couldn't been retrieved");
             }
