69ebd1f7cc087e5fd33d51f2eb0687b712adacb8
Stefano Cascavilla <cascavilla1@gmail.com>
2020-06-09 15:57:48 +0200

    Uploaded WebsiteInfo class definition

--- S/RemoteS.java
+++ S/RemoteS.java
@@ -1,3 +1,5 @@
+package S;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
---
+++ S/WebsiteInfo.java
@@ -0,0 +1,18 @@
+package S;
+
+import java.io.Serializable;
+
+public class WebsiteInfo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String url;
+    private String webPageContentHTML;
+
+    public WebsiteInfo(String url, String webPageContentHTML) {
+        this.url = url;
+        this.webPageContentHTML = webPageContentHTML;
+    }
+
+    public String getUrl() { return this.url; }
+
+    public String getWebPageContentHTML() { return this.webPageContentHTML; }
+}
